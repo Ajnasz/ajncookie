e6b264d0e331489d7f713bc2e821ccc3c624158f
Lajos Koszti <ajnasz@ajnasz.hu>
2012-05-28 17:05:18 +0200

    Handle domain, secure and httpOnly flags

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 /*jslint node: true*/
 function getCookie(req, cookieName) {
     var cookies = {},
+        cn,
         output;
     if (req.headers.cookie) {
         req.headers.cookie.split(';').forEach(function (cookie) {
             var parts = cookie.split('=');
-            cookies[parts[0].trim()] = parts[1].trim();
+            if (parts.length > 1) {
+                cn = parts[0].trim();
+                cookies[cn] = parts[1].trim();
+            }
+            /* else { */
+              /* it may be secure, or http only */
+              /*
+              switch (parts[0].trim()) {
+              case 'HttpOnly':
+                  httpOnly = true;
+                  break;
+              case 'Secure':
+                  secure = true;
+                  break;
+              }
+            }*/
         });
         output = cookies[cookieName];
     }
     return output;
 }
 
-function setCookie(res, cookie, val, expdays) {
+function setCookie(res, cookie, val, expdays, domain, secure, httpOnly) {
     var cookieVal = cookie + '=' + val,
-        expdate;
+        expdate,
+        cookies;
 
     if (expdays) {
         expdays = parseInt(expdays, 10);
@@ -25,11 +42,33 @@ function setCookie(res, cookie, val, expdays) {
         }
     }
 
+    if (domain) {
+        cookieVal += '; Domain=' + domain;
+    }
+
+    if (secure) {
+        cookieVal += '; Secure';
+    }
+
+    if (httpOnly) {
+        cookieVal += '; HttpOnly';
+    }
+
+    if (res.getHeader('Set-Cookie')) {
+        // current cookies must be kept
+        cookies = res.getHeader('Set-Cookie');
+        if (typeof cookies === 'string') {
+            cookies = [cookies];
+        }
+        cookies.push(cookieVal);
+        cookieVal = cookies;
+    }
+
     res.setHeader('Set-Cookie', cookieVal);
 }
 
-function delCookie(res, cookie) {
-    setCookie(res, cookie, "", -1);
+function delCookie(res, cookie, domain, secure, httpOnly) {
+    setCookie(res, cookie, "", -1, domain, secure, httpOnly);
 }
 
 exports.getCookie = getCookie;
